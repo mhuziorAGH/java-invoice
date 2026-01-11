dc318101d32b14d40355d26b6132b6d97d54113d
huzior <huzior@d17-426a-pc-13.ipa.ii.agh.edu.pl>
2026-01-11 15:38:00 +0100

    New pass tests

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,50 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+//    private Collection<Product> products
+//            = new ArrayList<Product>();
+
+    private Map<Product, Integer> products = new HashMap<Product, Integer>();
+
 
     public void addProduct(Product product) {
-        // TODO: implement
+        this.addProduct(product, 1);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        this.products.put(product, quantity);
     }
 
-    public BigDecimal getSubtotal() {
-        return null;
+    public BigDecimal getNetValue() {
+        BigDecimal value = BigDecimal.ZERO;
+        // dla kardego produktu po mapie
+        //dodaj cene do value
+
+        for (Product product : this.products.keySet()) {
+            Integer quantity = this.products.get(product);
+            BigDecimal price = product.getPrice();
+            price = price.multiply(BigDecimal.valueOf(quantity));
+            value = value.add(price);
+        }
+
+        return value;
     }
 
     public BigDecimal getTax() {
-        return null;
+
+        return BigDecimal.ZERO;
     }
 
-    public BigDecimal getTotal() {
-        return null;
+    public BigDecimal getGrossValue() {
+
+        return BigDecimal.ZERO;
     }
 }
